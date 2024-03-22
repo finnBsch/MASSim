bf7fc5e3a2a6d6c2360b7d4ce272da280dfc8ae1
finn <finn.lukas.busch@gmail.com>
2024-03-22 15:05:35 +0100

    minor speed improvements

--- src/mas_sim/mas_sim.cpp
+++ src/mas_sim/mas_sim.cpp
@@ -6,6 +6,8 @@
 #include "vector"
 #include <iostream>
 
+#include <chrono>
+using namespace std::chrono;
 MASSim::MASSim(int n_agents):
         n_agents(n_agents),
         agents()
@@ -20,12 +22,19 @@ MASSim::MASSim(int n_agents):
 }
 
 void MASSim::step() {
+    auto start = high_resolution_clock::now();
     for (auto& agent: agents) {
         agent->calculateMotion();
     }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    std::cout << "After motion update " << duration.count() <<std::endl;
     for (auto& agent: agents) {
         agent->step();
     }
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    std::cout << "After step " << duration.count() <<std::endl;
     bool collision_free = false;
     while(!collision_free) {
         collision_free = true;
@@ -33,7 +42,9 @@ void MASSim::step() {
             collision_free = false;
         }
     }
-
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    std::cout << "After step " << duration.count() <<std::endl;
 }
 
 
@@ -53,28 +64,32 @@ bool MASSim::correctAgentPairs() {
         for (auto& pair: agent_pairs){
             int i = pair[0];
             int j = pair[1];
+            float col_distance = agents[i]->getRadius() + agents[j]->getRadius();
+
             auto pose1 = agents[i]->getPose();
             auto pose2 = agents[j]->getPose();
-            double x1 = pose1(0,0);
-            double y1 = pose1(1, 0);
-            double x2 = pose2(0, 0);
-            double y2 = pose2(1, 0);
-            double dx = x2 - x1;
-            double dy = y2 - y1;
-            double d = sqrt(dx * dx + dy * dy);
-            float col_distance = agents[i]->getRadius() + agents[j]->getRadius();
-            if(d < col_distance) {
-                double difference = col_distance - d + 0.01;
-                // Correct the position such that the distance is collision_radius
-                x1 -= 0.5*difference * dx / d;
-                y1 -= 0.5*difference * dy / d;
-                x2 += 0.5*difference * dx / d;
-                y2 += 0.5*difference * dy / d;
-                collision_free = false;
-                required_correction = true;
+            if(abs(pose1(0) - pose2(0)) < col_distance && abs(pose1(1) - pose2(1)) < col_distance) {
+
+                double x1 = pose1(0, 0);
+                double y1 = pose1(1, 0);
+                double x2 = pose2(0, 0);
+                double y2 = pose2(1, 0);
+                double dx = x2 - x1;
+                double dy = y2 - y1;
+                double d = sqrt(dx * dx + dy * dy);
+                if (d < col_distance) {
+                    double difference = col_distance - d + 0.01;
+                    // Correct the position such that the distance is collision_radius
+                    x1 -= 0.5 * difference * dx / d;
+                    y1 -= 0.5 * difference * dy / d;
+                    x2 += 0.5 * difference * dx / d;
+                    y2 += 0.5 * difference * dy / d;
+                    collision_free = false;
+                    required_correction = true;
+                }
+                agents[i]->correctPose(x1, y1);
+                agents[j]->correctPose(x2, y2);
             }
-            agents[i]->correctPose(x1, y1);
-            agents[j]->correctPose(x2, y2);
         }
     }
     return required_correction;
